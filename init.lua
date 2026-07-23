8bf97734834235fa6824231252a85e98bfa68a63
Jatin Dalal <jd.jatindalal@gmail.com>
2026-07-23 16:08:21 +0530

    some options

--- init.lua
+++ init.lua
@@ -211,7 +211,7 @@ local telescope = {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", ";f", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", ";r", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", ";;", builtin.current_buffer_fuzzy_find, { desc = "search in current buffer" })
+		vim.keymap.set("n", ";l", builtin.current_buffer_fuzzy_find, { desc = "search in current buffer" })
 		vim.keymap.set("n", ";b", builtin.buffers, { desc = "[S]earch [B]uffers" })
 		vim.keymap.set({ "n" }, ";c", function()
 			vim.cmd("e " .. vim.fn.stdpath("config") .. "/init.lua")
@@ -233,7 +233,7 @@ local neotree = {
 			close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
 			-- popup_border_style = "rounded",
 			enable_git_status = true,
-			enable_diagnostics = true,
+			enable_diagnostics = false,
 			open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
 			default_component_configs = {
 				indent = {
