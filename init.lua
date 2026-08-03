47fb8291a2759a601ba60d35d9ef0544c9f089e3
Jatin Dalal <jd.jatindalal@gmail.com>
2026-08-03 17:16:40 +0530

    remove treesitter use lsp code fold

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ vim.opt.listchars = {
 }
 vim.o.cmdheight = 0
 vim.o.foldmethod = "expr"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.opt.fillchars:append({ eob = " " })
@@ -337,7 +337,6 @@ enabled_plugins = {
 	"lsp",
 	"debugger",
 	"ai",
-	"treesitter",
 }
 
 local plugins = {}
@@ -1076,59 +1075,6 @@ plugins.ai = {
 			"nvim-telescope/telescope.nvim",
 		},
 	},
-}
-
-plugins.treesitter = {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	priority = 1000,
-	build = ":TSUpdate",
-	config = function()
-		local ts = require("nvim-treesitter")
-		ts.setup({})
-		local ensure_installed = {
-			"lua",
-			"python",
-			"javascript",
-			"typescript",
-			"vimdoc",
-			"vim",
-			"regex",
-			"terraform",
-			"sql",
-			"dockerfile",
-			"toml",
-			"json",
-			"java",
-			"groovy",
-			"go",
-			"gitignore",
-			"graphql",
-			"yaml",
-			"make",
-			"cmake",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"tsx",
-			"css",
-			"html",
-			"cpp",
-		}
-
-		ts.install(ensure_installed)
-		require("nvim-treesitter.install").prefer_git = true
-
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function(args)
-				pcall(vim.treesitter.start, args.buf)
-				local ok = pcall(vim.treesitter.indentexpr)
-				if ok then
-					vim.bo[args.buf].indentexpr = "v:lua.vim.treesitter.indentexpr()"
-				end
-			end,
-		})
-	end,
 }
 
 local active_plugins = {}
